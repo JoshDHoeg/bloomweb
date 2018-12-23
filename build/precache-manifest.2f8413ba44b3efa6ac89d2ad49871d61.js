28cae27bd00850b2c9c59ad5069ad5ea28580635
Josh Hoeg <josh.hoeg@du.edu>
2018-12-23 14:54:34 -0700

    initial build

---
+++ build/precache-manifest.2f8413ba44b3efa6ac89d2ad49871d61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2af7b1af6797370ac6dd",
+    "url": "/static/js/main.2af7b1af.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2af7b1af6797370ac6dd",
+    "url": "/static/css/main.e37f2885.chunk.css"
+  },
+  {
+    "revision": "d0ac2236854b4f9e1de6642341065c99",
+    "url": "/index.html"
+  }
+];
